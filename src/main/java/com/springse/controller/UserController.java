9ae3e33c27da195335687c6f285c1f4efe94350c
Kumar <Dinesh.Kumar28@dellteam.com>
2023-10-17 02:02:23 +0530

    class name modified

--- src/main/java/com/springse/controller/UserController.java
+++ src/main/java/com/springse/controller/UserController.java
@@ -3,7 +3,7 @@ package com.springse.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +11,12 @@ import com.springse.model.User;
 import com.springse.service.UserService;
 
 @RestController
-public class TestController {
+public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping("/users")
 	public List<User> users() {
 		return userService.getUsers();
