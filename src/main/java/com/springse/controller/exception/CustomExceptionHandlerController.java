e09df6045faea1e51fc5c7350d311c0f8ac49159
Kumar <Dinesh.Kumar28@dellteam.com>
2023-10-17 12:15:16 +0530

    class name modified

---
+++ src/main/java/com/springse/controller/exception/CustomExceptionHandlerController.java
@@ -0,0 +1,24 @@
+package com.springse.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.springse.model.exception.ErrorModel;
+
+@ControllerAdvice
+public class CustomExceptionHandlerController{
+
+	@ExceptionHandler(IndexOutOfBoundsException.class)
+	public final ResponseEntity<Object> handleIndexOutOfBoundException(Exception ex, WebRequest request) throws Exception {
+		
+		ErrorModel errorModel = new ErrorModel();
+		errorModel.setDetails("--customer details");
+		errorModel.setMessage("--customer message without extending any class--");
+		
+		return new ResponseEntity<Object>(errorModel, HttpStatus.NOT_FOUND);
+	}	
+}
