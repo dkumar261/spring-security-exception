78edc3ba32ede8c5900638a8baf96618eaa55909
dineshnextq <v.dinesh.kumar@nextq.ai>
2023-11-17 04:17:46 +0530

    api gateway feature.

--- src/main/java/com/sx/config/SecurityConfig.java
+++ src/main/java/com/sx/config/SecurityConfig.java
@@ -12,9 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.sx.filter.JwtRequestFilter;
 import com.sx.service.UserServiceToken;
 import com.sx.util.ApplicationConstants;
 
@@ -26,9 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserServiceToken userServiceToken;
 
-	@Autowired
-	private JwtRequestFilter jwtRequestFilter;
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().cors().disable().authorizeRequests()
@@ -36,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authenticationProvider(daoAuthenticationProvider());
 
-		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		//http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 //	@Override
